544a30ea23a67ede6c2dec78aa5b7dd2d3205dc2
Bindu1511 <141307648+Bindu1511@users.noreply.github.com>
2024-11-12 11:45:42 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Userprofile from './components/Userprofile';
+
+
+const App=() => {
+  return (
+   <div>
+     <Userprofile />
+     </div>
+   );
+};
+
+export default App;
